c2826373d6506b819f342f22a37dae5108e587b2
Thalita Alencar <125590093+thallencar@users.noreply.github.com>
2025-07-11 17:01:15 +0000

    feat: add GET /rooms

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import {
     type ZodTypeProvider
 } from 'fastify-type-provider-zod'
 import { env } from './env.ts'
+import { getRoomsRoute } from './db/http/routes/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -19,5 +20,7 @@ app.setValidatorCompiler(validatorCompiler)
 app.get('/health', () => {
     return 'OK'
 })
+
+app.register(getRoomsRoute)
 
 app.listen({ port: env.PORT })
